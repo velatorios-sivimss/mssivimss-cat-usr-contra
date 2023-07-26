bc4ca764dc4b142fd1c0fa28d7dc83e0f58dbd0a
EduardoGlez16 <105452946+EduardoGlez16@users.noreply.github.com>
2023-07-26 14:09:06 -0600

    ajuste para resolver null pointer al actualizar

--- src/main/java/com/imss/sivimss/usuarioscontratantes/beans/UsrContra.java
+++ src/main/java/com/imss/sivimss/usuarioscontratantes/beans/UsrContra.java
@@ -173,6 +173,7 @@ public class UsrContra {
 				"SPA.DES_PAIS",
 				"CASE "
 				+ "WHEN SP.ID_PAIS=119 THEN 'Mexicana' "
+				+"WHEN SP.ID_PAIS IS NULL THEN '' "
 				+ "ELSE 'Extranjera' END "
 				+ "AS NACIONALIDAD",
 				"SP.ID_ESTADO",
@@ -369,11 +370,16 @@ public class UsrContra {
 		.from(SVC_PERSONA)
 		.join("SVC_CONTRATANTE SV", "SP.ID_PERSONA = SV.ID_PERSONA");
 		queryUtil.where("SP.NOM_PERSONA= :nombre").and("SP.NOM_PRIMER_APELLIDO= :paterno")
-		.and("SP.NOM_SEGUNDO_APELLIDO= :materno").and("SP.CVE_RFC= :rfc").and("SP.ID_PERSONA != :id")
+		.and("SP.NOM_SEGUNDO_APELLIDO= :materno")
+		.and("SP.ID_PERSONA != :id")
 		.setParameter("nombre", nombre).setParameter("paterno", paterno).setParameter("materno", materno)
-		.setParameter("rfc", rfc).setParameter("id", idPersona);
+		.setParameter("id", idPersona);
+		if(rfc!=null) {
+			queryUtil.where("SP.CVE_RFC= :rfc")
+			.setParameter("rfc", rfc);
+		}
 		String query = obtieneQuery(queryUtil);
-		log.info("-> " +query);
+		log.info("-> validacion" +query);
 		String encoded = encodedQuery(query);
 			parametro.put(AppConstantes.QUERY, encoded);
 				request.setDatos(parametro);
